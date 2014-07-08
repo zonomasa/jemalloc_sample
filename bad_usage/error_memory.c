2b5ceb9ddd8516d9fc7dc0011cf50fdf27c81906
zonomasa <tatezono@gmail.com>
2014-07-08 16:11:12 +0900

    Add double-free detect test

--- bad_usage/error_memory.c
+++ bad_usage/error_memory.c
@@ -60,9 +60,7 @@ bad_free(void){
 
     ptr = (char *)malloc(32);
 
-
-    (char *)ptr++;
-
+    ptr++;
 
     free(ptr);
 
@@ -106,6 +104,24 @@ overflow(void) {
         ptr[i] = 'a';
     }
     free(ptr);
+
+    /* OK */
+/*
+<jemalloc>: Corrupt redzone 1 byte before 0x2b0908c0f081 (size 32), byte=0x64
+<jemalloc>: Corrupt redzone 2 bytes before 0x2b0908c0f081 (size 32), byte=0x64
+<jemalloc>: Corrupt redzone 3 bytes before 0x2b0908c0f081 (size 32), byte=0x64
+<jemalloc>: Corrupt redzone 4 bytes before 0x2b0908c0f081 (size 32), byte=0x64
+<jemalloc>: Corrupt redzone 5 bytes before 0x2b0908c0f081 (size 32), byte=0x64
+<jemalloc>: Corrupt redzone 6 bytes before 0x2b0908c0f081 (size 32), byte=0x64
+<jemalloc>: Corrupt redzone 7 bytes before 0x2b0908c0f081 (size 32), byte=0x64
+*/
+
+    ptr = (char *)malloc(24);
+    for (i = 0; i > -8; i--){
+        ptr[i] = 'd';
+    }
+    free(ptr);
+
 
     return;
 }
