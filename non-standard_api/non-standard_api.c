a278d56e8d3cbb6c2df50fb5a79bd7471d645549
zonomasa <tatezono@gmail.com>
2014-08-24 20:19:39 +0900

    Update non-standard api sample

--- non-standard_api/non-standard_api.c
+++ non-standard_api/non-standard_api.c
@@ -2,20 +2,63 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#ifdef USE_JEMALLOC
 #include <jemalloc/jemalloc.h>
-#endif
+#include "../jemalloc/include/jemalloc/internal/arena.h"
+
+#define JEMALLOC_H_TYPES
+typedef struct arena_s arena_t;
+extern arena_t **arenas;
 
 void
 test_mallocx()
 {
     void *ptr;
 
+    /* MALLOCX_LG_ALIGN(x), x should be power of 2 */
     /* align ptr address with multiple of 2^11 */
     ptr = (void *)mallocx(3, MALLOCX_LG_ALIGN(11));
     if (ptr != NULL){
         printf("ptr : %p \n", ptr);
         assert(((unsigned long)ptr % (1 << 11)) == 0);
+    }else{
+        fprintf(stderr, "ptr is NULL\n");
+    }
+
+    /* MALLOCX_ALIGN(x), x should be power of 2 */
+    /* align ptr address with multiple of 2^11 */
+    ptr = (void *)mallocx(3, MALLOCX_ALIGN(2048));
+    if (ptr != NULL){
+        printf("ptr : %p \n", ptr);
+        assert(((unsigned long)ptr % (1 << 11)) == 0);
+    }else{
+        fprintf(stderr, "ptr is NULL\n");
+    }
+
+    /* MALLOCX_ZERO */
+    /* initialize region by 0 */
+    ptr = (void *)mallocx(1000,MALLOCX_ZERO);
+    if (ptr != NULL){
+        printf("ptr : %p \n", ptr);
+        printf("size : %zu \n", malloc_usable_size(ptr));
+        for (int i = 0; i < 1024/ sizeof(int);i++)
+            printf("%d", *((int*)ptr + i));
+//        assert(malloc_usable_size(ptr) == 0);
+//        assert(((unsigned long)ptr % (1 << 11)) == 0);
+    }else{
+        fprintf(stderr, "ptr is NULL\n");
+    }
+
+    /* MALLOCX_ARENA(a), a is index of arena */
+    /* specify the arena by a */
+    /* TODO This option is maybe invalid when thread-cache is enabled */
+    ptr = (void *)mallocx(8,MALLOCX_ARENA(2));
+    if (ptr != NULL){
+        printf("ptr : %p \n", ptr);
+        printf("size : %zu \n", malloc_usable_size(ptr));
+
+//        printf("%p\n", arenas[2]);
+//        assert(malloc_usable_size(ptr) == 0);
+//        assert(((unsigned long)ptr % (1 << 11)) == 0);
     }else{
         fprintf(stderr, "ptr is NULL\n");
     }
